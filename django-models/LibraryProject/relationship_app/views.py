4b2fad1776864c60aa981ffa94c4a2496c4cbc0b
Emmanuel Ayo <avinnewealth@gmail.com>
2025-11-28 14:27:17 +0100

    Add script for role based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 # -------------------------
 # Function-Based View
@@ -68,3 +70,33 @@ def register_view(request):
         form = UserCreationForm()
 
     return render(request, "relationship_app/register.html", {"form": form})
+
+# ---------------------------
+# Role Check Functions
+# ---------------------------
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+# ---------------------------
+# Role-Based Views
+# ---------------------------
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
