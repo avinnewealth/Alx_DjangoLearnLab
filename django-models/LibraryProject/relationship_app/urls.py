14b98275156c4c992e10f8cda429eef6eff0bb44
Emmanuel Ayo <avinnewealth@gmail.com>
2025-12-09 13:36:19 +0100

    update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,8 +10,17 @@ urlpatterns = [
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
     # Auth paths
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+ path(
+        "login/",
+        LoginView.as_view(template_name="registration/login.html"),
+        name="login"
+    ),
+
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="registration/logout.html"),
+        name="logout"
+    ),
     path("register/", views.register_view, name="register"),
 
     # Role-based dashboard paths
