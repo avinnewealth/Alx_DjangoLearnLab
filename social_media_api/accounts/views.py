462bfff2b0d5eceff28505d8518135a225eb77dd
Emmanuel Ayo <avinnewealth@gmail.com>
2025-12-20 15:22:56 +0100

    Initialize Social Media API with custom user model and token authentication

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
+from .serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    UserProfileSerializer
+)
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.create(user=user)
+            return Response({
+                'token': token.key,
+                'user': serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key
+        })
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data)
